9b9a3b1f3e1a796c02b413943d1939e3c67a5ad0
AgrntP <hn80577@gmail.com>
2025-12-01 02:46:02 +0700

    check status + update ui

---
+++ client/my-app/src/component/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import './TicTacToe .css'
+import circle from '../assets/circle.png'
+import cross from '../assets/cross.png'
+
+export const TicTacToe = () => {
+  return (
+    <div className='container'>
+        <h1 className="title">Tic Tac Toe </h1>
+        <div className="board">
+            
+        
+        
+        </div>
+        <button className="reset">Restart</button>
+    </div>
+  )
+}
